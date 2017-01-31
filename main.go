639fce6004253e74d3a0a0fa2f0215bbf8a28afa
TheoRev <hrevillafernandez79@gmail.com>
2017-01-30 20:56:52 -0500

    Creación de un API Rest básica Se creo un API Rest basica con GorilaMux como enrutador.

--- main.go
+++ main.go
@@ -1,5 +1,111 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/gorilla/mux"
+)
+
+var noteStore = make(map[string]Note)
+var id int
+
+func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/api/notes", GetNoteHandler).Methods("GET")
+	r.HandleFunc("/api/notes", PostNoteHandler).Methods("POST")
+	r.HandleFunc("/api/notes/{id}", PutNoteHandler).Methods("PUT")
+	r.HandleFunc("/api/notes/{id}", DeleteNoteHandler).Methods("DELETE")
+
+	server := &http.Server{
+		Addr:           ":1700",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Println("Listening http://localhost:1700")
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// GetNoteHandler method|
+func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var notes []Note
+	for _, v := range noteStore {
+		notes = append(notes, v)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(notes)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+// PostNoteHandler method
+func PostNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var note Note
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		panic(err)
+	}
+	note.CreateAt = time.Now()
+	id++
+	k := strconv.Itoa(id)
+	noteStore[k] = note
+
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(note)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
+
+// PutNoteHandler method
+func PutNoteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+	var noteUpdate Note
+	err := json.NewDecoder(r.Body).Decode(&noteUpdate)
+	if err != nil {
+		panic(err)
+	}
+
+	if note, ok := noteStore[k]; ok {
+		noteUpdate.CreateAt = note.CreateAt
+		delete(noteStore, k)
+		noteStore[k] = noteUpdate
+	} else {
+		log.Printf("No se encontró el Id: %s", k)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// DeleteNoteHandler method
+func DeleteNoteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+
+	if _, ok := noteStore[k]; ok {
+		delete(noteStore, k)
+	} else {
+		log.Printf("No se encontró el Id: %s", k)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+//Note es una estructura de para las notas
+type Note struct {
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreateAt    time.Time `json:"create_at"`
 }
